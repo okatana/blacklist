f0545e58236ae17e4226fc9c17df6f24100b6468
okatana <okatana17@gmail.com>
2019-01-17 18:03:38 +0300

    правка стилей для кнопок excel

---
+++ excelDMS.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katanugina
+ * Date: 17.01.2019
+ * Time: 15:10
+ */
+require 'config/config.php';
+require 'php/BlackListController.php';
+
+// контроллер
+$controller = new BlackListController($config);
+$vids = 'dms';
+$controller->excel($vids);
